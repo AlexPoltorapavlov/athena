d1fb715603fa5ac90395a051346e9891e00943ec
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-07-11 11:42:16 +0300

    cancancan is working

--- Gemfile
+++ Gemfile
@@ -69,3 +69,5 @@ end
 gem "dotenv-rails", "~> 3.1"
 
 gem "devise", "~> 4.9"
+
+gem "cancancan", "~> 3.6"
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :admin_panel
+
   # Defines the root path route ("/")
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root 'admin_panel#index'
 
 end
