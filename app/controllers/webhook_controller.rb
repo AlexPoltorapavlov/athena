034099731929eb409b7382497f5614001aa2d3ac
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-07-19 10:30:44 +0300

    remove debugging info from webhook_controller

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -20,27 +20,12 @@ class WebhookController < Telegram::Bot::UpdatesController
     bot_status = update['my_chat_member']['new_chat_member']['status']
     chat_info = update['my_chat_member']['chat']
 
-    puts "Вывод chat_info: \n #{chat_info}"
-
     if bot_status == 'member'
-      puts 'Вход в условие с bot_status == \'member\''
       chat = Chat.new( chat_id: chat_info['id'], chat_name: chat_info['title']+chat_info['id'].to_s, group: nil )
-      puts 'Было создание сущности чата.'
-      if chat.save!
-        puts 'Чат сохранен'
-      end
+      chat.save!
     elsif bot_status == 'left'
       chat = Chat.find_by(chat_id: chat_info['id'])
-      if chat
-        puts "Попытка удалить чат с ID #{chat_info['id']}..."
-        if chat.destroy
-          puts "Чат с ID #{chat_info['id']} успешно удален."
-        else
-          puts "Не удалось удалить чат с ID #{chat_info['id']}. Ошибки: #{chat.errors.full_messages.join(', ')}"
-        end
-      else
-        puts "Чат с ID #{chat_info['id']} не найден."
-      end
+      chat.destroy!
     end
 
   end
@@ -58,33 +43,24 @@ class WebhookController < Telegram::Bot::UpdatesController
   # Be sure to use splat args and default values to not get errors when
   # someone passed more or less arguments in the message.
   def start!(word = nil, *other_words)
-    puts "\n\nStart! method called\n\n"
     user = find_user_by_name(from['username'])
-    puts "\n\nUser found: #{user.id}\n\n" if user
 
     if user.nil?
       login = SecureRandom.hex(4)
       password = SecureRandom.hex(4)
 
-      puts "\n\nCreating new user with login: #{login} and password: #{password}\n\n"
-
       @user = User.new(email: login, password: password, telegram_link: from['username'],
                        username: login, login: login )
 
       if @user.save
-        puts "\n\nUser saved successfully\n\n"
         response = "Приветствую! Данные для входа: \n
         Логин: \"#{login}\" \n
         Пароль: \"#{password}\" \n
         Приятного пользования!"
       else
-        puts "\n\nFailed to save user: #{@user.errors.full_messages.join(', ')}\n\n"
-
         response = "Приветствую! Я не смогла создать тебе аккаунт, попробуй запустить меня еще раз."
       end
     else
-      puts "\n\nWelcome back, #{from['first_name']}!\n\n"
-
       response = "С возвращением, #{from['first_name']}!"
     end
 
@@ -126,8 +102,8 @@ class WebhookController < Telegram::Bot::UpdatesController
     User.find_by(telegram_link: name)
   end
 
-  def is_chat?
-    puts ('сработал is_chat')
-  end
+  # def is_chat?
+  #   puts ('сработал is_chat')
+  # end
 
 end
