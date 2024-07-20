f87b5d2cee1c692d9152feb6dcf5527bcd12cdb6
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-07-20 19:44:33 +0300

    Chats can be edited

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,31 @@
 class ChatsController < ApplicationController
+
+  before_action :set_chat, only: %i[show edit update]
+
   def index
     @chats = Chat.all
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @chat.update(chat_params)
+      redirect_to chats_path, notice: 'Чат успешно обновлен'
+    else
+      render :edit, alert: @chat.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  def set_chat
+    @chat = Chat.find(params[:id])
+  end
+
+  def chat_params
+    params.require(:chat).permit(:chat_name)
   end
 
 end
